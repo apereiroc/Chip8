59a4c061a38e910d0eae6726a72a4079e49b190b
Asier Pereiro <asier.pereiro.castro@cern.ch>
2021-04-21 11:18:25 +0200

    Add screen class

---
+++ screen.cpp
@@ -0,0 +1,79 @@
+//
+// Created by Asier Pereiro Castro on 30/1/21.
+//
+
+#include "screen.h"
+
+screen::screen()
+{
+
+}
+
+screen::~screen()
+{
+    // Don't worry if you forgot to close... I'll do it!
+	if (!_isclosed)
+        close();
+}
+
+bool screen::init(const std::string& name)
+{
+    // Initialization flag
+    bool success = true;
+
+    // Initialize SDL
+    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        success = false;
+        return success;
+    }
+    
+    // Create window
+    _window = SDL_CreateWindow( (const char*) name.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if ( _window == nullptr )
+    {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        success = false;
+        return success;
+    }
+    
+
+    return success;
+}
+
+bool screen::init()
+{   
+    return this->init("CHIP-8 EMULATOR");
+}
+
+void screen::close()
+{
+    // Free texture
+    if (_texture != nullptr)
+    {
+        SDL_DestroyTexture( _texture );
+        _texture = nullptr;
+    }
+
+    // Free renderer
+    if (_renderer != nullptr)
+    {
+        SDL_DestroyRenderer( _renderer );
+        _renderer = nullptr;
+    }
+
+    // Free window
+    if (_window != nullptr)
+    {
+        SDL_DestroyWindow( _window );
+        _window = nullptr;
+    }
+
+	// Clean up all initialised subsystems
+	SDL_Quit();
+
+    // Flip the flag to not close twice
+    _isclosed = true;
+}
